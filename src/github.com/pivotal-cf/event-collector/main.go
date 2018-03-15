4eea656f80b3b7f36b42078698a38bd0966e5ffb
Greg Patricio <gpatricio@pivotal.io>
2018-03-15 10:48:25 +0000

    Post tile version to configured endpoint
    [#155894041]
    
    Signed-off-by: Vlad Stoian <vstoian@pivotal.io>

--- src/github.com/pivotal-cf/event-collector/main.go
+++ src/github.com/pivotal-cf/event-collector/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"time"
 )
@@ -14,8 +17,40 @@ func main() {
 	fmt.Println("PKS Tile Version: ", pksTileVersion)
 	fmt.Println("Aggregator Endpoint: ", aggregatorEndpoint)
 
+	sendEvent(aggregatorEndpoint, pksTileVersion)
+
 	for {
-		time.Sleep(10 * time.Second)
+		time.Sleep(time.Minute)
 	}
 
+}
+
+func sendEvent(url, pksTileVersion string) {
+	var jsonStr = []byte(fmt.Sprintf(`{"product_version":"%s"}`, pksTileVersion))
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	fmt.Println("Request Headers:", req.Header)
+	fmt.Println("Request Body:", req.Body)
+
+	client := &http.Client{
+		Timeout: time.Minute,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response Status:", resp.Status)
+	fmt.Println("Response Headers:", resp.Header)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Response Body:", string(body))
 }
